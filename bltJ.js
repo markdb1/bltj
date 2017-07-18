042e1273cd61452e7ae57976d2ccccbab2a857f4
baycat <anyssa.cast@gmail.com>
2017-07-18 14:51:02 -0500

    blt todo

--- bltJ.js
+++ bltJ.js
@@ -17,3 +17,56 @@ var y = d.getFullYear();
 var s =d.getDate();
 document.getElementById("m1").innerHTML = n;
 document.getElementById("d1").innerHTML = s;
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
+// close button to hide current list item
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+// checked symbol when clicked
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev){
+  if (ev.target.tagName === 'LI'){
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+// create a new list item when add is clicked
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("Please write something.");
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
