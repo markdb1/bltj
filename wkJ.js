a8dec5fa5e739b32c0de63c8582b8b2882223263
baycat <anyssa.cast@gmail.com>
2017-07-19 10:43:35 -0500

    aesthetic

--- wkJ.js
+++ wkJ.js
@@ -78,3 +78,160 @@ function newElement() {
     }
   }
 }
+
+function newElement2() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput2").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("Please write something.");
+  } else {
+    document.getElementById("myUL2").appendChild(li);
+  }
+  document.getElementById("myInput2").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
+
+function newElement3() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput3").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("Please write something.");
+  } else {
+    document.getElementById("myUL3").appendChild(li);
+  }
+  document.getElementById("myInput3").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
+
+
+function newElement4() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput4").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("Please write something.");
+  } else {
+    document.getElementById("myUL4").appendChild(li);
+  }
+  document.getElementById("myInput4").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
+
+function newElement5() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput5").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("Please write something.");
+  } else {
+    document.getElementById("myUL5").appendChild(li);
+  }
+  document.getElementById("myInput5").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
+
+function newElement6() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput6").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("Please write something.");
+  } else {
+    document.getElementById("myUL6").appendChild(li);
+  }
+  document.getElementById("myInput6").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
+
+function newElement7() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput7").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("Please write something.");
+  } else {
+    document.getElementById("myUL7").appendChild(li);
+  }
+  document.getElementById("myInput7").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
