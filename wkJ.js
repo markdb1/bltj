288d6238163608df0a8990fffa1b19c574dc2f8d
baycat <anyssa.cast@gmail.com>
2017-07-19 09:19:02 -0500

    week files

---
+++ wkJ.js
@@ -0,0 +1,80 @@
+var d = new Date();
+var month = new Array();
+month[0] = "January";
+month[1] = "February";
+month[2] = "March";
+month[3] = "April";
+month[4] = "May";
+month[5] = "June";
+month[6] = "July";
+month[7] = "August";
+month[8] = "September";
+month[9] = "October";
+month[10] = "November";
+month[11] = "December";
+var n = month[d.getMonth()];
+var y = d.getFullYear();
+document.getElementById("mnth").innerHTML = n;
+document.getElementById("yr").innerHTML = y;
+var currentSeconds = $.now().getSeconds();
+//trying to set up alerts
+if(d == eventTime){
+  alert("test");
+}
+
+// taskbar css
+// close button and appending it to each item
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
+// close button to hide current list item
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+// checked symbol when clicked
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev){
+  if (ev.target.tagName === 'LI'){
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+// create a new list item when add is clicked
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("Please write something.");
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
