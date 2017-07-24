e5044657e37a39f2775b1f5d6bfb4c8cbe0c1431
Katevanphilip <katevanphilip@gmail.com>
2017-07-24 13:30:02 -0500

    notes colors

--- homeJ.js
+++ homeJ.js
@@ -103,7 +103,6 @@ function newElement() {
 var quotes = [
   "Don't cry because it's over, smile because it happened. -Dr. Seuss",
   "Be yourself; everyone else is already taken. -Oscar Wilde",
-  "You've gotta dance like there's nobody watching, Love like you'll never be hurt, Sing like there's nobody listening, And live like it's heaven on earth. -William W. Purkey",
   "A room without books is like a body without a soul. -Marcus Tullius Cicero",
   "You know you're in love when you can't fall asleep because reality is finally better than your dreams. -Dr. Seuss",
   "You only live once, but if you do it right, once is enough. -Mae West",
